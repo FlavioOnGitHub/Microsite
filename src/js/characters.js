b056449d745a101b77dca22328c516d6ddc8631b
FlavioOnGitHub <flaviorotunno@gmail.com>
2024-11-14 14:21:46 +0100

    Working on finishing the character animation

--- src/js/characters.js
+++ src/js/characters.js
@@ -1,3 +1,5 @@
+import { gsap } from "gsap";
+
 const claudeCard = document.querySelector(
   ".character-section-card:nth-of-type(1)"
 );
@@ -11,11 +13,6 @@ const ysaCard = document.querySelector(
   ".character-section-card:nth-of-type(2)"
 );
 const allCards = document.querySelector(".character-section-card-container");
-const allPopups = document.querySelectorAll(".character-section-pop-up");
-const claudePopup = document.querySelector(".character-section-pop-up-claude");
-const baronPopup = document.querySelector(".character-section-pop-up-baron");
-const roiPopup = document.querySelector(".character-section-pop-up-roi");
-const ysaPopup = document.querySelector(".character-section-pop-up-ysa");
 const characterHeading = document.querySelector(".character-section-heading");
 const characterButton = document.querySelectorAll(".button-character");
 
@@ -39,6 +36,9 @@ let ysaStatus = 0;
 let baronStatus = 0;
 let roiStatus = 0;
 
+const tlAppear = gsap.timeline();
+const tlDiseappear = gsap.timeline();
+
 function lockScroll() {
   document.body.classList.add("lock-scroll");
   window.scrollTo({
@@ -54,64 +54,519 @@ function unlockScroll() {
 
 claudeCard.addEventListener("click", function () {
   lockScroll();
-  allCards.classList.add("character-section-blur");
-  characterHeading.classList.add("character-section-blur");
-  claudePopup.classList.add("character-section-pop-up-visible");
+  tlAppear
+    .to(allCards, {
+      opacity: 0.3,
+      filter: "blur(20px)",
+      duration: 0.2,
+      pointerEvents: "none",
+    })
+    .to(
+      characterHeading,
+      {
+        opacity: 0.3,
+        filter: "blur(20px)",
+        duration: 0.2,
+        pointerEvents: "none",
+      },
+      "<"
+    )
+    .to(
+      ".character-section-pop-up-claude .character-section-pop-up-heading span:nth-child(1)",
+      { x: 0, duration: 1 }
+    )
+    .to(".character-section-pop-up-claude p", { x: 0, duration: 1 }, "<")
+    .to(
+      ".character-section-pop-up-claude .character-section-pop-up-image-1",
+      {
+        x: 0,
+        duration: 1,
+        opacity: 1,
+        delay: 0.5,
+      },
+      "<"
+    )
+    .to(".character-section-pop-up-claude .character-section-pop-up-image-2", {
+      x: 0,
+      duration: 0.5,
+      opacity: 1,
+    })
+    .to(".character-section-pop-up-claude .character-section-pop-up-image-3", {
+      x: 0,
+      duration: 0.5,
+      opacity: 1,
+    })
+    .to(".character-section-pop-up-claude .character-section-pop-up-image-4", {
+      x: 0,
+      duration: 0.5,
+      opacity: 1,
+    })
+    .to(".character-section-pop-up-claude a", {
+      x: 0,
+      opacity: 1,
+      duration: 1,
+    });
   claudeStatus = 1;
 });
 
 baronCard.addEventListener("click", function () {
   lockScroll();
-  allCards.classList.add("character-section-blur");
-  characterHeading.classList.add("character-section-blur");
-  baronPopup.classList.add("character-section-pop-up-visible");
+  tlAppear
+    .to(allCards, {
+      opacity: 0.3,
+      filter: "blur(20px)",
+      duration: 0.2,
+      pointerEvents: "none",
+    })
+    .to(
+      characterHeading,
+      {
+        opacity: 0.3,
+        filter: "blur(20px)",
+        duration: 0.2,
+        pointerEvents: "none",
+      },
+      "<"
+    )
+    .to(
+      ".character-section-pop-up-baron .character-section-pop-up-heading span:nth-child(1)",
+      { x: 0, duration: 1 }
+    )
+    .to(".character-section-pop-up-baron p", { x: 0, duration: 1 }, "<")
+    .to(
+      ".character-section-pop-up-baron .character-section-pop-up-image-1",
+      {
+        x: 0,
+        duration: 1,
+        opacity: 1,
+        delay: 0.5,
+      },
+      "<"
+    )
+    .to(".character-section-pop-up-baron .character-section-pop-up-image-2", {
+      x: 0,
+      duration: 0.5,
+      opacity: 1,
+    })
+    .to(".character-section-pop-up-baron .character-section-pop-up-image-3", {
+      x: 0,
+      duration: 0.5,
+      opacity: 1,
+    })
+    .to(".character-section-pop-up-baron .character-section-pop-up-image-4", {
+      x: 0,
+      duration: 0.5,
+      opacity: 1,
+    })
+    .to(".character-section-pop-up-baron a", {
+      x: 0,
+      opacity: 1,
+      duration: 1,
+    });
   baronStatus = 1;
 });
 
 roiCard.addEventListener("click", function () {
   lockScroll();
-  allCards.classList.add("character-section-blur");
-  characterHeading.classList.add("character-section-blur");
-  roiPopup.classList.add("character-section-pop-up-visible");
+  tlAppear
+    .to(allCards, {
+      opacity: 0.3,
+      filter: "blur(20px)",
+      duration: 0.2,
+      pointerEvents: "none",
+    })
+    .to(
+      characterHeading,
+      {
+        opacity: 0.3,
+        filter: "blur(20px)",
+        duration: 0.2,
+        pointerEvents: "none",
+      },
+      "<"
+    )
+    .to(
+      ".character-section-pop-up-roi .character-section-pop-up-heading span:nth-child(1)",
+      { x: 0, duration: 1 }
+    )
+    .to(".character-section-pop-up-roi p", { x: 0, duration: 1 }, "<")
+    .to(
+      ".character-section-pop-up-roi .character-section-pop-up-image-1",
+      {
+        x: 0,
+        duration: 1,
+        opacity: 1,
+        delay: 0.5,
+      },
+      "<"
+    )
+    .to(".character-section-pop-up-roi .character-section-pop-up-image-2", {
+      x: 0,
+      duration: 0.5,
+      opacity: 1,
+    })
+    .to(".character-section-pop-up-roi .character-section-pop-up-image-3", {
+      x: 0,
+      duration: 0.5,
+      opacity: 1,
+    })
+    .to(".character-section-pop-up-roi .character-section-pop-up-image-4", {
+      x: 0,
+      duration: 0.5,
+      opacity: 1,
+    })
+    .to(".character-section-pop-up-roi a", {
+      x: 0,
+      opacity: 1,
+      duration: 1,
+    });
   roiStatus = 1;
 });
 
 ysaCard.addEventListener("click", function () {
   lockScroll();
-  allCards.classList.add("character-section-blur");
-  characterHeading.classList.add("character-section-blur");
-  ysaPopup.classList.add("character-section-pop-up-visible");
+  tlAppear
+    .to(allCards, {
+      opacity: 0.3,
+      filter: "blur(20px)",
+      duration: 0.2,
+      pointerEvents: "none",
+    })
+    .to(
+      characterHeading,
+      {
+        opacity: 0.3,
+        filter: "blur(20px)",
+        duration: 0.2,
+        pointerEvents: "none",
+      },
+      "<"
+    )
+    .to(
+      ".character-section-pop-up-ysa .character-section-pop-up-heading span:nth-child(1)",
+      { x: 0, duration: 1 }
+    )
+    .to(".character-section-pop-up-ysa p", { x: 0, duration: 1 }, "<")
+    .to(
+      ".character-section-pop-up-ysa .character-section-pop-up-image-1",
+      {
+        x: 0,
+        duration: 1,
+        opacity: 1,
+        delay: 0.5,
+      },
+      "<"
+    )
+    .to(".character-section-pop-up-ysa .character-section-pop-up-image-2", {
+      x: 0,
+      duration: 0.5,
+      opacity: 1,
+    })
+    .to(".character-section-pop-up-ysa .character-section-pop-up-image-3", {
+      x: 0,
+      duration: 0.5,
+      opacity: 1,
+    })
+    .to(".character-section-pop-up-ysa .character-section-pop-up-image-4", {
+      x: 0,
+      duration: 0.5,
+      opacity: 1,
+    })
+    .to(".character-section-pop-up-ysa a", {
+      x: 0,
+      opacity: 1,
+      duration: 1,
+    });
   ysaStatus = 1;
 });
 
 characterButton.forEach(function (button) {
   button.addEventListener("click", function (event) {
     event.preventDefault();
-    allPopups.forEach(function (popup) {
-      popup.classList.remove("character-section-pop-up-visible");
-    });
-    allCards.classList.remove("character-section-blur");
-    characterHeading.classList.remove("character-section-blur");
-    window.scrollTo({
-      top: 1900,
-      behavior: "smooth",
-    });
-
     if (claudeStatus === 1) {
+      tlDiseappear
+        .to(
+          ".character-section-pop-up-claude .character-section-pop-up-heading span:nth-child(1)",
+          { x: -1500, duration: 0.5 }
+        )
+        .to(
+          ".character-section-pop-up-claude .character-section-pop-up-image-1",
+          { x: -1500, duration: 0.3, opacity: 1 },
+          "<"
+        )
+        .to(
+          ".character-section-pop-up-claude p",
+          { x: 1000, duration: 0.5 },
+          "<"
+        )
+        .to(
+          ".character-section-pop-up-claude .character-section-pop-up-image-2",
+          { x: -1500, duration: 0.3, opacity: 1 },
+          "<"
+        )
+        .to(
+          ".character-section-pop-up-claude .character-section-pop-up-image-3",
+          { x: -1500, duration: 0.3, opacity: 1 },
+          "<"
+        )
+        .to(
+          ".character-section-pop-up-claude .character-section-pop-up-image-4",
+          { x: -1500, duration: 0.3, opacity: 1 },
+          "<"
+        )
+
+        .to(
+          ".character-section-pop-up-claude a",
+          {
+            x: 1000,
+            opacity: 0,
+            duration: 0.5,
+          },
+          "<"
+        )
+
+        .to(
+          allCards,
+          {
+            opacity: 1,
+            filter: "blur(0)",
+            duration: 0.2,
+            pointerEvents: "all",
+          },
+          "+=0"
+        )
+        .to(
+          characterHeading,
+          {
+            opacity: 1,
+            filter: "blur(0)",
+            duration: 0.2,
+            pointerEvents: "none",
+            onComplete: function () {
+              tlAppear.clear();
+              window.scrollTo({
+                top: 1900,
+                behavior: "smooth",
+              });
+            },
+          },
+          "<"
+        );
       hiddenClaude.classList.add("character-section-image-reveal");
+      claudeStatus = 0;
     }
 
     if (ysaStatus === 1) {
+      tlDiseappear
+        .to(
+          ".character-section-pop-up-ysa .character-section-pop-up-heading span:nth-child(1)",
+          { x: -1500, duration: 0.5 }
+        )
+        .to(
+          ".character-section-pop-up-ysa .character-section-pop-up-image-1",
+          { x: -1500, duration: 0.3, opacity: 1 },
+          "<"
+        )
+        .to(".character-section-pop-up-ysa p", { x: 1000, duration: 0.5 }, "<")
+        .to(
+          ".character-section-pop-up-ysa .character-section-pop-up-image-2",
+          { x: -1500, duration: 0.3, opacity: 1 },
+          "<"
+        )
+        .to(
+          ".character-section-pop-up-ysa .character-section-pop-up-image-3",
+          { x: -1500, duration: 0.3, opacity: 1 },
+          "<"
+        )
+        .to(
+          ".character-section-pop-up-ysa .character-section-pop-up-image-4",
+          { x: -1500, duration: 0.3, opacity: 1 },
+          "<"
+        )
+
+        .to(
+          ".character-section-pop-up-ysa a",
+          {
+            x: 1000,
+            opacity: 0,
+            duration: 0.5,
+          },
+          "<"
+        )
+
+        .to(
+          allCards,
+          {
+            opacity: 1,
+            filter: "blur(0)",
+            duration: 0.2,
+            pointerEvents: "all",
+          },
+          "+=0"
+        )
+        .to(
+          characterHeading,
+          {
+            opacity: 1,
+            filter: "blur(0)",
+            duration: 0.2,
+            pointerEvents: "none",
+            onComplete: function () {
+              tlAppear.clear();
+              window.scrollTo({
+                top: 1900,
+                behavior: "smooth",
+              });
+            },
+          },
+          "<"
+        );
       hiddenYsa.classList.add("character-section-image-reveal");
+      ysaStatus = 0;
     }
 
     if (baronStatus === 1) {
+      tlDiseappear
+        .to(
+          ".character-section-pop-up-baron .character-section-pop-up-heading span:nth-child(1)",
+          { x: -1500, duration: 0.5 }
+        )
+        .to(
+          ".character-section-pop-up-baron .character-section-pop-up-image-1",
+          { x: -1500, duration: 0.3, opacity: 1 },
+          "<"
+        )
+        .to(
+          ".character-section-pop-up-baron p",
+          { x: 1000, duration: 0.5 },
+          "<"
+        )
+        .to(
+          ".character-section-pop-up-baron .character-section-pop-up-image-2",
+          { x: -1500, duration: 0.3, opacity: 1 },
+          "<"
+        )
+        .to(
+          ".character-section-pop-up-baron .character-section-pop-up-image-3",
+          { x: -1500, duration: 0.3, opacity: 1 },
+          "<"
+        )
+        .to(
+          ".character-section-pop-up-baron .character-section-pop-up-image-4",
+          { x: -1500, duration: 0.3, opacity: 1 },
+          "<"
+        )
+
+        .to(
+          ".character-section-pop-up-baron a",
+          {
+            x: 1000,
+            opacity: 0,
+            duration: 0.5,
+          },
+          "<"
+        )
+
+        .to(
+          allCards,
+          {
+            opacity: 1,
+            filter: "blur(0)",
+            duration: 0.2,
+            pointerEvents: "all",
+          },
+          "+=0"
+        )
+        .to(
+          characterHeading,
+          {
+            opacity: 1,
+            filter: "blur(0)",
+            duration: 0.2,
+            pointerEvents: "none",
+            onComplete: function () {
+              tlAppear.clear();
+              window.scrollTo({
+                top: 1900,
+                behavior: "smooth",
+              });
+            },
+          },
+          "<"
+        );
       hiddenBaron.classList.add("character-section-image-reveal");
+      baronStatus = 0;
     }
 
     if (roiStatus === 1) {
+      tlDiseappear
+        .to(
+          ".character-section-pop-up-roi .character-section-pop-up-heading span:nth-child(1)",
+          { x: -1500, duration: 0.5 }
+        )
+        .to(
+          ".character-section-pop-up-roi .character-section-pop-up-image-1",
+          { x: -1500, duration: 0.3, opacity: 1 },
+          "<"
+        )
+        .to(".character-section-pop-up-roi p", { x: 1000, duration: 0.5 }, "<")
+        .to(
+          ".character-section-pop-up-roi .character-section-pop-up-image-2",
+          { x: -1500, duration: 0.3, opacity: 1 },
+          "<"
+        )
+        .to(
+          ".character-section-pop-up-roi .character-section-pop-up-image-3",
+          { x: -1500, duration: 0.3, opacity: 1 },
+          "<"
+        )
+        .to(
+          ".character-section-pop-up-roi .character-section-pop-up-image-4",
+          { x: -1500, duration: 0.3, opacity: 1 },
+          "<"
+        )
+
+        .to(
+          ".character-section-pop-up-roi a",
+          {
+            x: 1000,
+            opacity: 0,
+            duration: 0.5,
+          },
+          "<"
+        )
+
+        .to(
+          allCards,
+          {
+            opacity: 1,
+            filter: "blur(0)",
+            duration: 0.2,
+            pointerEvents: "all",
+          },
+          "+=0"
+        )
+        .to(
+          characterHeading,
+          {
+            opacity: 1,
+            filter: "blur(0)",
+            duration: 0.2,
+            pointerEvents: "none",
+            onComplete: function () {
+              tlAppear.clear();
+              window.scrollTo({
+                top: 1900,
+                behavior: "smooth",
+              });
+            },
+          },
+          "<"
+        );
       hiddenRoi.classList.add("character-section-image-reveal");
+      roiStatus = 0;
     }
+
     unlockScroll();
     nav.classList.remove("nav-scroll-locked");
   });
