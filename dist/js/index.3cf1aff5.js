0ff6dc2586daa12ff25427f69d8435fd806d44cb
FlavioOnGitHub <flaviorotunno@gmail.com>
2024-11-28 15:38:44 +0100

    commit dist

---
+++ dist/js/index.3cf1aff5.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",()=>{let e=document.querySelector(".nav"),n=0;window.addEventListener("scroll",()=>{let d=window.scrollY;d>n&&d>100?e.classList.add("nav-scrolling-down"):e.classList.remove("nav-scrolling-down"),n=d})});
+//# sourceMappingURL=index.3cf1aff5.js.map
